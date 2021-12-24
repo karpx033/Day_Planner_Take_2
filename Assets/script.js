852d73718a5f67e4e4d9c17d39a6addb82ad9640
Louis <karpx033@umn.edu>
2021-12-23 19:01:56 -0600

    all times now css appropriately

--- Assets/script.js
+++ Assets/script.js
@@ -9,56 +9,21 @@ for (const Number of colors){
 }
 var today = moment();
 $("#currentDay").text(today.format("dddd, MMMM Do"));
-console.log(Number(parseInt(moment().startOf('day').fromNow())));
-console.log(typeof Number(parseInt(moment().startOf('day').fromNow())));
-var nine = ($("#9"))
-nine.value= 18;
-
-// i = 9
-
-// document.getElementById([i]).classList.add("present")
 
 function colorcoder() {
     var currenthour = Number(parseInt(moment().startOf('day').fromNow()));
-    console.log(currenthour);
-    console.log(typeof currenthour);
-    console.log(nine.value);
-    console.log(typeof nine.value);
-   
-
     var timeEL = document.getElementsByClassName("timeCL");
+    console.log(currenthour);
     for (var i=0; i<timeEL.length; i++){
         if (currenthour === Number([i])) {
          timeEL[i].classList.add("present");
-    //     } else if (currenthour <[i]) {
-    //         document.getElementById((Number([i]))).classList.add("present"); 
-    //     } else {
-    //         document.getElementById((Number([i]))).classList.add("present");
-    //     }
-    // }
-     
-     
-     
-     
-        // timeEL[i].style.display = "";
-}}};    
-//    for (let i = 0; i < 4; i++) {
-//     console.log([i]);
-//     console.log(Number([i]));
-//     console.log(typeof (Number([i])));
-//     console.log(document.getElementById(Number([i])));
-//     document.getElementById(Number([i])).classList.add("present");
-//     if (currenthour === Number([i])) {
-        
-//         document.getElementById((Number([i]))).classList.add("present");
-//     } else if (currenthour <[i]) {
-//         document.getElementById((Number([i]))).classList.add("present"); 
-//     } else {
-//         document.getElementById((Number([i]))).classList.add("present");
-//     }
-// }
-// // }
-//    }
-// }
-
+        } else if (currenthour <Number([i])) {
+            timeEL[i].classList.add("future"); 
+        } else {
+            timeEL[i].classList.add("past");
+        }
+    }
+}
+  
 colorcoder();
+
