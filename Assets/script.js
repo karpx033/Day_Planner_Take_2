66ac3511298822561561059368e06cb7f72e7fbb
Louis <karpx033@umn.edu>
2021-12-23 22:38:02 -0600

    all inputs now save to local storage

--- Assets/script.js
+++ Assets/script.js
@@ -1,19 +1,12 @@
-// var timeblocks = $(".container" );
-// var timetable =document.createElement("UL");
-// var tablerow =document.createElement("LI");
-// var hours = "";
 
-let colors = ['nine', 'ten', 'eleven'];
-for (const Number of colors){
-    console.log(Number);
-}
+
 var today = moment();
 $("#currentDay").text(today.format("dddd, MMMM Do"));
 
 function colorcoder() {
     var currenthour = Number(parseInt(moment().startOf('day').fromNow()));
     var timeEL = document.getElementsByClassName("timeCL");
-    console.log(currenthour);
+   
     for (var i=0; i<timeEL.length; i++){
         if (currenthour === Number([i])) {
          timeEL[i].classList.add("present");
@@ -27,32 +20,23 @@ function colorcoder() {
   
 colorcoder();
 
-// var todos = {}
-
-
-// function toLS(){
-//   var input = document.getElementById("textarea");
-//   localStorage.setItem("description", (input.value)); 
-  
-// }
-
-
 
 
 function saving(event) {
-    console.log(event);
+    // console.log(event);
     var buttonEL = document.getElementsByTagName("button");
     let current = event.target.parentNode.parentNode;
     let prevSiblings = current.previousElementSibling;
     var thetarget = prevSiblings.children[0];
-    console.log(thetarget);
-    console.log(thetarget.value);
+    // console.log(thetarget);
+    // console.log(thetarget.value);
     var data =thetarget.value;
-    console.log(data);
-    console.log(buttonEL);
-    console.log(event.target.parentNode);
+    // console.log(data);
+    // console.log(buttonEL);
+    // console.log(event.target.parentNode);
     for (var i=0; i<buttonEL.length; i++){
-        if (buttonEL[i] === event.target.parentNode) {
+        if (buttonEL[i].id === event.target.parentNode.id) {
     localStorage.setItem("todo " +[i], JSON.stringify(data));
-        } else return;
-}}
+        }    
+}};
+
